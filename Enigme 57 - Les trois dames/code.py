5aa66a27e92a39ef0e70899e4f977e927b115a58
Serge Mimault <s.mimault@monaco-telecom.mc>
2025-06-25 17:35:36 +0200

    Clean comments

--- Enigme 57 - Les trois dames/code.py
+++ Enigme 57 - Les trois dames/code.py
@@ -1,23 +1,11 @@
 '''
 Enigme 57 - Les trois dames
-
-Index de la grille 5x5
-+----+----+----+----+----+
-|  0 |  1 |  2 |  3 |  4 |
-+----+----+----+----+----+
-|  5 |  6 |  7 |  8 |  9 |
-+----+----+----+----+----+
-| 10 | 11 | 12 | 13 | 14 |
-+----+----+----+----+----+
-| 15 | 16 | 17 | 18 | 19 |
-+----+----+----+----+----+
-| 20 | 21 | 22 | 23 | 24 |
-+----+----+----+----+----+
 '''
 from math import ceil
 from typing import Iterable
 from functools import partial
 
+# La grille des scores attendus une fois les dames placées
 expected_scores = (
     [ 2, 2, 3, 1, 1 ]
   + [ 2, 1, 2, 2, 3 ]
@@ -63,6 +51,19 @@ class ScoreGrid:
     '''
     Classe de manipulation de la grille des dames.
     Met à disposition le calcul des scores de la grille pour chaque dame ajoutée.
+    
+    Index de la grille 5x5 :
+    +----+----+----+----+----+
+    |  0 |  1 |  2 |  3 |  4 |
+    +----+----+----+----+----+
+    |  5 |  6 |  7 |  8 |  9 |
+    +----+----+----+----+----+
+    | 10 | 11 | 12 | 13 | 14 |
+    +----+----+----+----+----+
+    | 15 | 16 | 17 | 18 | 19 |
+    +----+----+----+----+----+
+    | 20 | 21 | 22 | 23 | 24 |
+    +----+----+----+----+----+
     '''
     def __init__(self, *indexes: int):
         '''
