e609cd72d19e56bece057258ca6ff5bd2061a581
Alien <alien@alien-station.net>
2025-06-28 18:19:56 +0200

    #30 solved

--- Enigme 30 - Les noyaux/code.py
+++ Enigme 30 - Les noyaux/code.py
@@ -1,58 +1,79 @@
-'''
-Enigme 30 : les noyaux
-'''
 import random
-from typing import Iterable, NamedTuple
+from typing import Hashable, Iterable, NamedTuple
 
-normal_stone = ( 'black', 'white' )
+basic_stone = ( 'black', 'white' )
 loaded_stone = ( 'black', 'black', 'black', 'white' )
-throws_amount = 1000000
 
-class ThrowResult(NamedTuple):
-    '''Un résultat est défini par le nombres de noyaux noirs et blancs'''
+class StonesThrower:
+    '''De quoi simuler le jet d'un ensemble de noyaux'''
+    def __init__(self, stones: Iterable) -> None:
+        self.stones = stones
+    
+    def throw(self, amount: int=1) -> tuple:
+        '''Retourne amount lancers de noyaux sous la forme d'un tuple contenant des tuples.
+        Chaque tuple comporte une série de couleurs tirées au hasard dans self.stones'''
+        output = [ ]
+        for _ in range(amount):
+            output.append(tuple([ random.choice(stone) for stone in self.stones ]))
+        return tuple(output)
+
+class Result(NamedTuple):
+    '''Résultat d'un lancer : nombre de noyaux noirs et blancs issus du lancer'''
     blacks: int
     whites: int
-
-def throws(amount: int, stone: Iterable) -> dict:
-    '''Retourne un dictionnaire associant la clé (nombre de noyaux noirs, nombre de noyaux blancs)
-    au nombre de lancers de cette combinaison'''
-    summary = { }
-    for t in range(amount):
-        result = tuple([ random.choice(stone[s]) for s in range(len(stone)) ])
-        index = ThrowResult(blacks=result.count('black'), whites=result.count('white'))
-        summary[index] = summary.get(index, 0) + 1
-    return summary
-
-def winratio(resultset: dict):
-    '''Retourne le pourcentage de victoires sur le resultset'''
-    total_throws = sum(resultset.values())
-    wins = sum([ resultset[result] for result in resultset if result.blacks % 2 != 0 ])
-    return 100 * wins / total_throws
-
-def show_results(resultset: dict) -> None:
-    print('------+--------+---------')
-    print('Noirs | Blancs | Lancers')
-    print('------+--------+---------')
-    for summary, counts in sorted(resultset.items()):
-        print(f'{summary.blacks:>5} | {summary.whites:>6} | {counts:>7}')
-    print('------+--------+---------')
     
+class Summary(dict):
+
+    def __missing__(self, key: Hashable) -> int:
+        '''On veut un compteur qui commence à zéro si l'entrée n'est pas initialisée. Facie !'''
+        return 0
+
+    def add(self, *throws: tuple) -> None:
+        '''Ajout d'un lancer au résumé
+        On garde une trace du nombre de noyaux noirs et blancs'''
+        for throw in throws:
+            result = Result(blacks=throw.count('black'), whites=throw.count('white'))
+            self[result] += 1
+
+    def __str__(self) -> str:
+        '''Retourne une chaîne représentant le résumé'''
+        hline = '------+--------+---------'
+        output = [ hline, 'Noirs | Blancs | Lancers', hline ]
+        output += [ f'{summary.blacks:>5} | {summary.whites:>6} | {amount:>7}' for summary, amount in sorted(self.items()) ]
+        output.append(hline)
+        return '\n'.join(output)
+    
+    @property
+    def throws(self) -> int:
+        '''Retourne le nombre de lancers'''
+        return sum(self.values())
+    
+    @property
+    def wins(self) -> int:
+        '''Retourne le nombre de victoires'''
+        return sum([ count for result, count in self.items() if result.blacks % 2 == 1 ])
+    
+    @property
+    def losses(self) -> int:
+        '''Retourne le nombre de défaites'''
+        return sum([ count for result, count in self.items() if result.blacks % 2 == 0 ])
+
+    @property
+    def winratio(self) -> float:
+        '''Retourne le taux de victoire'''
+        return round(self.wins * 100 / self.throws, 2)
+
+def simulate(throws: int, normal: int, loaded: int):
+    '''Simule throws lancers composés de normal noyaux non pipés et loaded noyaux pipés'''
+    stones = StonesThrower((basic_stone, ) * normal + (loaded_stone, ) * loaded )
+    summary = Summary()
+    summary.add(*stones.throw(throws))
+    print(f'{throws} lancers de {normal} noyaux non pipés et {loaded} noyaux pipés :')
+    print(summary)
+    print(f'Taux de victoire : {summary.winratio}%')
+    print()
 
 if __name__ == '__main__':
-    print('Lancer de 8 noyaux non pipés')
-    resultset = throws(throws_amount, [ normal_stone ] * 8)
-    show_results(resultset)
-    print(f'Taux de victoires : {winratio(resultset)} %')
-    print()
-
-    print('Lancer de 8 noyaux dont 4 pipés')
-    resultset = throws(throws_amount, [ normal_stone ] * 4 + [ loaded_stone ] * 4)
-    show_results(resultset)
-    print(f'Taux de victoires : {winratio(resultset)} %')
-    print()
-
-    print('Lancer de 8 noyaux pipés')
-    resultset = throws(throws_amount, [ loaded_stone ] * 8)
-    show_results(resultset)
-    print(f'Taux de victoires : {winratio(resultset)} %')
-
+    simulate(100000, 8, 0)
+    simulate(100000, 4, 4)
+    simulate(100000, 0, 8)
